4a405614d76e46e47f937504c843e0ba99c2102a
Nunes20-ai <nunesapaga98@gmail.com>
2025-05-10 20:30:20 +0100

    Novo projecto

---
+++ Meus Projetos/modal/script.js
@@ -0,0 +1,12 @@
+const abrir=document.getElementById("botao2");
+const modal=document.getElementById("modal");
+const fechar=document.getElementById("botao1");
+
+abrir.addEvertListener("click", function(){
+    modal.style.display="block";
+})
+
+function clicou(){
+    modal.style.display="block";
+
+}
